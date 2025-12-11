f2d4bff15df015c289febac408971540aadf8f51
Junparkk <phjun0324@gmail.com>
2025-12-11 15:48:17 +0900

    사이즈 변경

--- src/components/sections/Greeting.tsx
+++ src/components/sections/Greeting.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export function Greeting() {
   return (
-    <section className="flex flex-col items-center w-full px-6">
+    <section className="flex flex-col items-center w-full px-16">
       <Image
         src="/images/text.png"
         alt="인사말"
